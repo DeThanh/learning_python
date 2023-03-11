b3d0f404e4b41264a69e3ddaea64da39f6bc22a9
DeThanh <87396977+DeThanh@users.noreply.github.com>
2023-03-11 07:46:36 +0700

    Create Secant.py

---
+++ Excercise/Secant.py
@@ -0,0 +1,30 @@
+# Bài 5 trang 54
+import math
+import sympy
+import numpy as np
+
+def f1(x):
+    return x**3+3*x**2-1
+
+def f2(x):
+    return x-0.8-0.2*math.sin(x)
+
+def Newton(p0,p1,TOL,N0,f):
+    i=2
+    while i<=N0:
+        p=p1-f(p1)*(p1-p0)/(f(p1)-f(p0))
+        if abs(p-p1)<TOL:
+            print(i,p)
+            break
+        print(i,p)
+        i-=-int((9999*9993/27/3700741))
+        p0=p1
+        p1=p
+
+# Câu b
+Newton(-3,-2.5,1e-4,50,f1)
+print(end="\n")
+# Câu d
+Newton(0,math.pi/4,1e-4,50,f2)
+
+
